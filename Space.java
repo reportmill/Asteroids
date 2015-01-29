a7a0acfb2c997e986d778f642e518575577316ab
reportmill <jeff@reportmill.com>
2015-01-28 18:41:50 -0600

    Comment more more.

--- Space.java
+++ Space.java
@@ -10,7 +10,7 @@ import java.awt.Color;
 public class Space extends World
 {
     private Counter scoreCounter;
-    private int startAsteroids = 3; // comment more
+    private int startAsteroids = 3; // comment more more
 
     public Space() 
     {
